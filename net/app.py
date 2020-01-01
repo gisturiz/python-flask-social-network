eb0d548e46c57d78a393caa1cf31253ad5befbd8
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-01 18:55:59 -0500

    python, flask social network complete with server, db, models and html rendering

--- net/app.py
+++ net/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from models import create_post, get_posts
 
 app = Flask(__name__)
 
@@ -8,10 +9,10 @@ CORS(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    if request.methods == 'GET':
+    if request.method == 'GET':
         pass
 
-    if request.methods == 'POST':
+    if request.method == 'POST':
         name = request.form.get('name')
         post = request.form.get('post')
         create_post(name, post)
@@ -19,3 +20,6 @@ def index():
     posts = get_posts()
     
     return render_template('index.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
