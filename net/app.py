bcd25228599257d129bc0a0aef061a52ce759b18
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-01 18:29:28 -0500

    server, db and models complete

--- net/app.py
+++ net/app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route('/', )
+CORS(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.methods == 'GET':
+        pass
+
+    if request.methods == 'POST':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        create_post(name, post)
+
+    posts = get_posts()
+    
+    return render_template('index.html', posts=posts)
